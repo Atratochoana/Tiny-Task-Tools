4467c11a3db62d895d4feadabf9e91c64018d0de
Ralph <Ralphy.t.small@gmail.com>
2024-11-25 22:36:47 +0000

    added basic webhook function

--- src/webhook.py
+++ src/webhook.py
@@ -1,3 +1,16 @@
 from dotenv import dotenv_values
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import json
 
 config = dotenv_values(".env")
+
+webhook = DiscordWebhook(url=config["WEBHOOK"])
+
+embed = DiscordEmbed(title="**Macro Status**", description='- Times run: `0` - Times failed: `0` - Times remaing: `0`', color="03b2f8")
+
+
+webhook.add_embed(embed)
+response = webhook.execute()
+
+print(json.loads(response.content.decode())["webhook_id"])
+
